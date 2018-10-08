97540c49609eb9c3163b13888d8f5a7df5b5ae9f
Adam Rosenberg <adam.arosenberg@gmail.com>
2018-10-07 18:29:14 -0600

    adding sql

---
+++ order_statistics.py
@@ -0,0 +1,15 @@
+import random
+
+def quickselect(data, n):
+    return
+def select(data, left, right, index):
+    if right == left:
+        return data[left]
+
+    pivot_index = random.randint(left, right)
+
+
+
+
+data = [2, 3, -2, 6, 8, 0]
+print quickselect(data, 3)
